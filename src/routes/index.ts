7a5a52a562bd1eafd4429b8ae6df68167674d5fb
savio-84 <saviocarvalho01@hotmail.com>
2021-03-01 09:05:15 -0300

    learning DDD

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import appointmentsRouter from './appointmentsRouter';
-
-const router = Router();
-
-router.use('/appointments', appointmentsRouter);
-
-export default router;
